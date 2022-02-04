5c4d78b310d75eadc0277b86a32154359e08bd29
JaredDenmarMRemo <98570132+JaredDenmarMRemo@users.noreply.github.com>
2022-02-05 06:12:05 +0800

    Add files via upload

---
+++ SampleData.sql
@@ -0,0 +1,10 @@
+--SAMPLE DATA FOR ADMIN TABLE--
+
+insert into admin_table values ('A00001', 'zaira@gmail.com', '');
+
+--SAMPLE DATA FOR PRODUCT TABLE--
+
+insert into product_table values ('P00001', 'Laptop', 'CAT001', '16250', 'Laptop.jpg', '111');
+insert into product_table values ('P00002', 'Smartphone', 'CAT001', '12900', 'Smart.jpg', '99');
+insert into product_table values ('P00003', 'Printer', 'CAT001', '8000', 'Printer.jpg', '80');
+insert into product_table values ('P00004', 'Headphone', 'CAT001', '4000', 'Headphone.jpg', '100');
